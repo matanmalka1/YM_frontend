0649d3da69dfbf498cd4c57710359d1105db8d90
matanmalka1 <matan1391@gmail.com>
2026-06-28 09:46:22 +0300

    knip

--- src/constants/filterOptions.constants.ts
+++ src/constants/filterOptions.constants.ts
@@ -7,7 +7,6 @@ export const getResultTypeLabel = (resultType: string) => {
 export const ALL_STATUSES_OPTION = { value: '', label: 'כל הסטטוסים' } as const
 export const ALL_TYPES_OPTION = { value: '', label: 'כל הסוגים' } as const
 export const ALL_MONTHS_OPTION = { value: '', label: 'כל החודשים' } as const
-export const ALL_YEARS_OPTION = { value: '', label: 'כל השנים' } as const
 export const ALL_YEARS_URL_OPTION = { value: 'all', label: 'כל השנים' } as const
 export const ALL_CATEGORIES_OPTION = { value: '', label: 'כל הקטגוריות' } as const
 export const ALL_PERIODS_OPTION = { value: '', label: 'כל התקופות' } as const
